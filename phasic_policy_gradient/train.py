25f5a52bae3b452fe7f1e9685fc290f24cd0dcf5
Izzy Putterman <carlipp176@gmail.com>
2021-03-29 16:28:21 -0700

    Num envs and tensorboard logging

--- phasic_policy_gradient/train.py
+++ phasic_policy_gradient/train.py
@@ -6,14 +6,14 @@ from .impala_cnn import ImpalaEncoder
 from . import logger
 from .envs import get_venv
 
-def train_fn(env_name="coinrun",
+def train_fn(env_name="fruitbot",
     distribution_mode="easy",
     arch="dual",  # 'shared', 'detach', or 'dual'
     # 'shared' = shared policy and value networks
     # 'dual' = separate policy and value networks
     # 'detach' = shared policy and value networks, but with the value function gradient detached during the policy phase to avoid interference
     interacts_total=50_000_000,
-    num_envs=1,
+    num_envs=8,
     n_epoch_pi=1,
     n_epoch_vf=1,
     gamma=.999,
@@ -36,7 +36,7 @@ def train_fn(env_name="coinrun",
 
     print("num envs: ", num_envs)
     if log_dir is not None:
-        format_strs = ['csv', 'stdout'] if comm.Get_rank() == 0 else []
+        format_strs = ['csv', 'stdout', "tensorboard"] if comm.Get_rank() == 0 else []
         logger.configure(comm=comm, dir=log_dir, format_strs=format_strs)
 
     venv = get_venv(num_envs=num_envs, env_name=env_name, distribution_mode=distribution_mode)
@@ -80,7 +80,7 @@ def train_fn(env_name="coinrun",
 def main():
     parser = argparse.ArgumentParser(description='Process PPG training arguments.')
     parser.add_argument('--env_name', type=str, default='fruitbot')
-    parser.add_argument('--num_envs', type=int, default=1)
+    parser.add_argument('--num_envs', type=int, default=8)
     parser.add_argument('--n_epoch_pi', type=int, default=1)
     parser.add_argument('--n_epoch_vf', type=int, default=1)
     parser.add_argument('--n_aux_epochs', type=int, default=6)
