6b46aa50eca889d0110b8385decab310155b0ded
Izzy Putterman <carlipp176@gmail.com>
2021-05-10 11:55:21 -0700

    maybe final push

--- README.md
+++ README.md
@@ -15,13 +15,24 @@ Supported Pythons:
 
 ## Install
 
-You can get miniconda from https://docs.conda.io/en/latest/miniconda.html if you don't have it, or install the dependencies from [`environment.yml`](environment.yml) manually.
+We have been running on Google Colab with the following requirements:
+!pip install mpi4py==3.0.3 gym3==0.3.3 procgen==0.10.4 pandas==1.1.1 matplotlib==3.1.1 tensorflow==1.15.0
 
-```
-clone repo
-```
+It comes preinstalled with torch==1.8.1+cu101, but torch==1.4.0 also works and versions between.
+
+In addition one can use conda and the environment.yml file located in the repo.  We included a .ipynb for colab use that contains everything needed to run
 
 ## Reproduce and Visualize Results
-python -m phasic_policy_gradient.train
+python -m phasic_policy_gradient.train --log_dir [logging directory name] --num_levels [number of training levels, default=100] --data_aug [gray, cutout, cutout_color, flip, rotate, color_jitter, crop, None, default=None]
 
 
+
+## Logging Name Syntax
+Most of the runs are named using the following convention ppg_{num envs}_{num eval levels}_{num training levels}_{optional: data augmentation}
+So the run ppg_16_all_50_crop is ppg run with the crop augmentation run on 50 training levels and infinite evaluation levels starting at 50.  So training is done on levels [0, 49] and evaluation is on [50, infinity]
+
+In csv logs the key columns are: B: EpLenMean (Average length of an episode), C: EpRewMean (Average reward in training episodes), I: Misc/InteractCount (Number of environment steps), AG: EvalEpMean (Average reward in evaluation Episodes, collected less frequently)
+
+
+##Note
+The best_test_log is from ppg_16_all_50_cutout_color and test_run is the last column
---
+++ rl_project/test.py
@@ -0,0 +1,102 @@
+import argparse
+from mpi4py import MPI
+from . import ppg
+from . import torch_util as tu
+from .impala_cnn import ImpalaEncoder
+from . import logger
+from .envs import get_venv
+import torch
+from .roller import Roller
+import matplotlib.pyplot as plt
+import pickle as pkl
+
+def train_fn(env_name="fruitbot",
+    distribution_mode="easy",
+    arch="dual",  # 'shared', 'detach', or 'dual'
+    # 'shared' = shared policy and value networks
+    # 'dual' = separate policy and value networks
+    # 'detach' = shared policy and value networks, but with the value function gradient detached during the policy phase to avoid interference
+    interacts_total=10_000_000,
+    num_envs=1,
+    n_epoch_pi=1,
+    n_epoch_vf=1,
+    gamma=.999,
+    aux_lr=5e-4,
+    lr=5e-4,
+    nminibatch=1,
+    aux_mbsize=4,
+    clip_param=.2,
+    kl_penalty=0.0,
+    n_aux_epochs=6,
+    n_pi=32,
+    beta_clone=1.0,
+    vf_true_weight=1.0,
+    model_dir='results/tmp/ppg_16_10_gray',
+    start_level=0,
+    comm=None):
+    if comm is None:
+        comm = MPI.COMM_WORLD
+    tu.setup_dist(comm=comm)
+    tu.register_distributions_for_tree_util()
+
+    print("num envs: ", num_envs)
+
+    venv = get_venv(num_envs=num_envs, env_name=env_name, distribution_mode=distribution_mode, num_levels=1, start_level=0)
+    # print(venv, env_name, distribution_mode, venv.start_level, venv.num_levels)
+    # print(x)
+
+    model = torch.load('results/ppg_16_all_100' + "/model.jd")
+    # model = ppg.PhasicValueModel(venv.ob_space, venv.ac_space, enc_fn, arch=arch)
+    model.to(tu.dev())
+    tu.sync_params(model.parameters())
+    model.eval()
+    test_roller = Roller(
+          act_fn=model.act,
+          venv=venv,
+          initial_state=model.initial_state(venv.num),
+          keep_buf=100,
+          keep_non_rolling=None
+      )
+    
+    out = test_roller.multi_step(500)
+    x, y, z = test_roller.observe()
+    test_roller.multi_step(5000)
+    a, b, c = test_roller.observe()
+    pkl.dump({"images1":y, "images2":b}, open("datasingle.pkl", "wb"))
+    print(test_roller.episode_count)
+    print(test_roller.recent_eplens, len(test_roller.recent_eplens))
+    print(test_roller.recent_eprets)
+    print(sum(test_roller.recent_eprets)/100)
+      
+
+def main():
+    parser = argparse.ArgumentParser(description='Process PPG training arguments.')
+    parser.add_argument('--env_name', type=str, default='fruitbot')
+    parser.add_argument('--num_envs', type=int, default=16)
+    parser.add_argument('--n_epoch_pi', type=int, default=1)
+    parser.add_argument('--n_epoch_vf', type=int, default=1)
+    parser.add_argument('--n_aux_epochs', type=int, default=6)
+    parser.add_argument('--n_pi', type=int, default=32)
+    parser.add_argument('--clip_param', type=float, default=0.2)
+    parser.add_argument('--kl_penalty', type=float, default=0.0)
+    parser.add_argument('--arch', type=str, default='dual') # 'shared', 'detach', or 'dual'
+    parser.add_argument('--model', type=str)
+    parser.add_argument('--start', type=int, default=0)
+    args = parser.parse_args()
+
+    comm = MPI.COMM_WORLD
+
+    train_fn(
+        env_name=args.env_name,
+        num_envs=args.num_envs,
+        n_epoch_pi=args.n_epoch_pi,
+        n_epoch_vf=args.n_epoch_vf,
+        n_aux_epochs=args.n_aux_epochs,
+        n_pi=args.n_pi,
+        arch=args.arch,
+        model_dir = args.model,
+        start_level=args.start,
+        comm=comm)
+
+if __name__ == '__main__':
+    main()
